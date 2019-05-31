ee640b5dfb85ad8d5916caf5a4390265d705ebfc
vinicio.ochoa <vinicio.ochoa@eppetroecuador.ec>
2019-05-31 16:21:12 -0500

    cambio de paquete

--- constantes/CodigoRetorno.go
+++ constantes/CodigoRetorno.go
@@ -1,16 +1,16 @@
 package constantes
 
-import a "github.com/fochoac/go-model-ws/prueba"
+import "github.com/fochoac/go-model-ws/prueba"
 
-func Ok() a.CodigoRetorno {
-	return a.CodigoRetorno{Codigo: "1",
+func Ok() prueba.CodigoRetorno {
+	return prueba.CodigoRetorno{Codigo: "1",
 		Mensaje: "Ok"}
 }
-func NoData() a.CodigoRetorno {
-	return a.CodigoRetorno{Codigo: "2",
+func NoData() prueba.CodigoRetorno {
+	return prueba.CodigoRetorno{Codigo: "2",
 		Mensaje: "No existe la información solicitada"}
 }
-func Error() a.CodigoRetorno {
-	return a.CodigoRetorno{Codigo: "3",
+func Error() prueba.CodigoRetorno {
+	return prueba.CodigoRetorno{Codigo: "3",
 		Mensaje: "Error inesperado, Itente nuevamente más tarde"}
 }
